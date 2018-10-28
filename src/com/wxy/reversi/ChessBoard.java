c96e6bebe6513da86b1dedf6326aae09f226c72c
wxyxsx <wxyxsx@126.com>
2018-10-28 11:02:59 +0800

    Complete the logic and simple interface of Othello

---
+++ src/com/wxy/reversi/ChessBoard.java
@@ -0,0 +1,207 @@
+package com.wxy.reversi;
+
+public class ChessBoard {
+	private static final int SCALE = 8; // must be even number
+	private int board[][] = new int[SCALE][SCALE]; // checkerboard
+	// 0 space 1 white -1 black
+	private int cur; // current number of space
+	private int xf, yf; // searching result
+	private int player; // current player
+	private int white, black; // count the number of piece
+	private boolean iflock; // check for deadlock
+
+	// white first default
+	public ChessBoard() {
+		Reset(1);
+	}
+
+	// choose the order
+	public ChessBoard(int o) {
+		Reset(o);
+	}
+
+	// reset the game state
+	public void Reset(int s) {
+		cur = SCALE * SCALE - 4;
+		player = s;
+		white = 0;
+		black = 0;
+		iflock = false;
+		for (int i = 0; i < SCALE; i++) {
+			for (int j = 0; j < SCALE; j++) {
+				board[i][j] = 0;
+			}
+		}
+		board[SCALE / 2 - 1][SCALE / 2 - 1] = 1;
+		board[SCALE / 2][SCALE / 2] = 1;
+		board[SCALE / 2 - 1][SCALE / 2] = -1;
+		board[SCALE / 2][SCALE / 2 - 1] = -1;
+	}
+
+	// get the color of the piece
+	public int GetChess(int x, int y) {
+		return board[x][y];
+	}
+
+	// the number of white
+	public int GetWhite() {
+		SumChess();
+		return white;
+	}
+
+	// the number of black
+	public int GetBlack() {
+		SumChess();
+		return black;
+	}
+
+	// get current user
+	public int GetPlayer() {
+		return player;
+	}
+
+	private void SumChess() {
+		white = 0;
+		black = 0;
+		for (int i = 0; i < SCALE; i++) {
+			for (int j = 0; j < SCALE; j++) {
+				if (board[i][j] == 1) {
+					white++;
+				} else if (board[i][j] == -1) {
+					black++;
+				}
+			}
+		}
+	}
+
+	// Change the pieces between (x1, y1) (x2, y2) to the same color
+	private void change(int x1, int y1, int x2, int y2) {
+		// the direction between pieces
+		int dx = x2 - x1;
+		int dy = y2 - y1;
+		if (dx != 0) {
+			dx = dx > 0 ? 1 : -1;
+		}
+		if (dy != 0) {
+			dy = dy > 0 ? 1 : -1;
+		}
+		// change color one by one
+		int i = x1 + dx;
+		int j = y1 + dy;
+		int color = board[x1][y1];
+		while (i != x2 || j != y2) {
+			board[i][j] = color;
+			i += dx;
+			j += dy;
+		}
+	}
+
+	// Determine if the piece is within the bounds
+	private boolean judge(int x, int y) {
+		if (x < 0 || x > 7 || y < 0 || y > 7) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// Check if the current piece can be placed at a certain position
+	private boolean ifallow(int x, int y) {
+		if (board[x][y] != 0 || !search8(x, y)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// Search 8 directions
+	private boolean search8(int x, int y) {
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if (i == 0 && j == 0) {
+					continue;
+				}
+				if (search(x, y, i, j)) {
+					// if find one result
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	// Detect if the current player can act
+	private boolean ifaction() {
+		for (int i = 0; i < SCALE; i++) {
+			for (int j = 0; j < SCALE; j++) {
+				if (ifallow(i, j)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	// Search the current user color along the direction of dx, dy
+	private boolean search(int x, int y, int dx, int dy) {
+		int color = player;
+		int i = x + dx;
+		int j = y + dy;
+		while (judge(i, j)) {
+			if (board[i][j] == color) {
+				xf = i;
+				yf = j;
+				// If it is adjacent
+				if (i == x + dx && j == y + dy) {
+					return false;
+				} else {
+					return true;
+				}
+			} else if (board[i][j] == 0) {
+				// If there is a blank
+				return false;
+			}
+			i += dx;
+			j += dy;
+		}
+		return false;
+	}
+
+	// Check if the game is over
+	public boolean Check() {
+		if (cur == 0 || GetWhite() == 0 || GetBlack() == 0 || iflock == true) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// Determine if you can put a piece
+	public boolean PlaceChess(int x, int y) {
+		if (!ifallow(x, y)) {
+			return false;
+		}
+		cur -= 1;
+		board[x][y] = player;
+		iflock = false;
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if (i == 0 && j == 0) {
+					continue;
+				}
+				if (search(x, y, i, j)) {
+					change(x, y, xf, yf);
+				}
+			}
+		}
+		player = player == -1 ? 1 : -1;
+		// If you can’t act, it’s still moved by the original user.
+		if (!ifaction()) {
+			player = player == -1 ? 1 : -1;
+			if (!ifaction()) {
+				iflock = true;
+			}
+		}
+		return true;
+	}
+}
---
+++ src/com/wxy/reversi/GameUI.java
@@ -0,0 +1,142 @@
+package com.wxy.reversi;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class GameUI extends JFrame implements MouseListener, MouseMotionListener {
+	private static final long serialVersionUID = 1L;
+	private ChessBoard test;
+	int x, y;// Mouse coordinates
+
+	public GameUI() {
+		test = new ChessBoard(1);
+		this.setTitle("Reversi");
+		this.setSize(500, 500);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.repaint();
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+	}
+
+	public void paint(Graphics g) {
+		BufferedImage buf = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
+		Graphics g1 = buf.createGraphics();
+		g1.setColor(new Color(153, 153, 255));
+		g1.fill3DRect(20, 40, 400, 400, true);
+		// x y width height
+		for (int i = 1; i < 8; i++) {
+			g1.setColor(Color.WHITE);
+			g1.drawLine(20, 40 + i * 50, 20 + 400, 40 + i * 50);// drawing Horizontal line
+			// x1 y1 x2 y2
+			g1.drawLine(20 + i * 50, 40, 20 + i * 50, 40 + 400); // drawing Vertical line
+		}
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				// drawing black
+				if (test.GetChess(i, j) == -1) {
+					int tempX = i * 50 + 20;
+					int tempY = j * 50 + 40;
+					g1.setColor(Color.BLACK);
+					g1.fillOval(tempX, tempY, 50, 50);
+					g1.setColor(Color.BLACK);
+					g1.drawOval(tempX, tempY, 50, 50);
+				}
+
+				// drawing white
+				if (test.GetChess(i, j) == 1) {
+					int tempX = i * 50 + 20;
+					int tempY = j * 50 + 40;
+					g1.setColor(Color.WHITE);
+					g1.fillOval(tempX, tempY, 50, 50);
+					g1.setColor(Color.BLACK);
+					g1.drawOval(tempX, tempY, 50, 50);
+				}
+			}
+		}
+		g.drawImage(buf, 0, 0, this);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (test.Check()) {
+			x = e.getX();
+			y = e.getY();
+			if (x >= 20 && x <= 420 && y >= 40 && y <= 440) {
+				x = (x - 20) / 50;
+				y = (y - 40) / 50;
+				if (!test.PlaceChess(x, y)) {
+					JOptionPane.showMessageDialog(this, "棋子必须放在能提子的位置！");
+					return;
+				}
+				if (test.GetPlayer() == 1) {
+					this.setTitle("白棋行动");
+				} else {
+					this.setTitle("黑棋行动");
+				}
+				this.repaint();
+				// Check if the game is over
+				if (!test.Check()) {
+					int i = test.GetBlack();
+					int j = test.GetWhite();
+					String str = String.format("黑棋%d个，白棋%d个\n", i, j);
+					if (i > j) {
+						JOptionPane.showMessageDialog(this, str + "游戏结束，黑方胜利");
+					} else {
+						JOptionPane.showMessageDialog(this, str + "游戏结束，白方胜利");
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+//		int i = e.getXOnScreen();
+//		int j = e.getYOnScreen();
+//		String str = String.format("(%d,%d)", i, j);
+//		this.setTitle(str);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+
+	}
+
+	public static void main(String[] args) {
+		new GameUI();
+	}
+
+}
